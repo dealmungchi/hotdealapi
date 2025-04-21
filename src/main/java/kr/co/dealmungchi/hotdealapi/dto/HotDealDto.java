84684f79ce119e330367c1603c5c5a74f78bcec9
Junseok Shin <sjsage522@gmail.com>
2025-04-21 10:23:24 +0900

    Feat thumbnail

--- src/main/java/kr/co/dealmungchi/hotdealapi/dto/HotDealDto.java
+++ src/main/java/kr/co/dealmungchi/hotdealapi/dto/HotDealDto.java
@@ -3,6 +3,7 @@ package kr.co.dealmungchi.hotdealapi.dto;
 import java.time.LocalDateTime;
 
 import kr.co.dealmungchi.hotdealapi.domain.entity.HotDeal;
+import kr.co.dealmungchi.hotdealapi.domain.entity.Provider.ProviderType;
 import lombok.Builder;
 
 @Builder
@@ -18,11 +19,16 @@ public record HotDealDto(
     LocalDateTime updatedAt
 ) {
     public static HotDealDto fromEntity(HotDeal entity) {
+        String thumbnailLink = null;
+        if (entity.getThumbnailHash() != null && !ProviderType.BBASAK.equals(entity.getProvider().getProviderType())) { // TODO : worker 에서 레퍼러 설정으로 가능한지 확인
+            thumbnailLink = System.getenv("STATIC_URL") + "/thumbnail/" + entity.getThumbnailHash().substring(0, 2) + "/" + entity.getThumbnailHash();
+        }
+
         return HotDealDto.builder()
                 .id(entity.getId())
                 .title(entity.getTitle())
                 .link(entity.getLink())
-                .thumbnailLink(entity.getThumbnailLink())
+                .thumbnailLink(thumbnailLink)
                 .price(entity.getPrice())
                 .providerId(entity.getProviderId())
                 .viewCount(entity.getViewCount())
